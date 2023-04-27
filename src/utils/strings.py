5c25d53fc57a7a466481878d483ccc44c9d8d22f
MrTeferi <freethoughtleft@gmail.com>
2023-04-27 17:51:51 -0500

    refactor(utils.strings): Moved headless console to string utilities, updated console output utility functions

--- src/utils/strings.py
+++ src/utils/strings.py
@@ -1,13 +1,39 @@
 """
 Utility Helpers Module
 """
+# Standard Library Imports
+from enum import Enum
+from functools import cached_property
+from typing import Union, Optional
 import unicodedata
 import string
 
+# Third Party Imports
 import photoshop.api as ps
 from packaging.version import parse
 
-from src.constants import con
+"""
+STRING CLASSES
+"""
+
+
+class StrEnum(str, Enum):
+    """
+    Enum where the value is always a string.
+    """
+    def __str__(self) -> str:
+        return self.value
+
+    @cached_property
+    def value(self) -> str:
+        return str(self._value_)
+
+
+class ConsoleMessages(StrEnum):
+    error = "#a84747"
+    warning = "#d4c53d"
+    success = "#59d461"
+
 
 """
 STRING UTILITIES
@@ -44,22 +70,68 @@ def ps_version_check(check_version: str) -> bool:
     return False
 
 
-def msg_error(msg: str) -> str:
+def is_multiline(text: Union[str, list[str]]) -> Union[bool, list[bool]]:
+    """
+    Check if text or list of texts given contains multiline text (a newline character).
+    @param text: String to check or list of strings to check.
+    @return: True/False or list of True/False values.
+    """
+    # String Given
+    if isinstance(text, str):
+        if '\n' in text or '\r' in text:
+            return True
+        return False
+    # List Given
+    if isinstance(text, list):
+        return [bool('\n' in t or '\r' in t) for t in text]
+    # Invalid data type provided
+    raise Exception("Invalid type passed to 'is_multiline', can only accept a string or list of strings.\n"
+                    f"Received the value: {text}")
+
+
+"""
+CONSOLE COLORS
+"""
+
+
+def msg_bold(msg: str) -> str:
+    """
+    Wraps a console string with italics tags.
+    @param msg: Message to wrap.
+    @return: Wrapped message.
+    """
+    return f"[b]{msg}[/b]"
+
+
+def msg_italics(msg: str) -> str:
+    """
+    Wraps a console string with italics tags.
+    @param msg: Message to wrap.
+    @return: Wrapped message.
+    """
+    return f"[i]{msg}[/i]"
+
+
+def msg_error(msg: str, reason: Optional[str] = None) -> str:
     """
     Adds unified error color tag to Proxyshop console message.
     @param msg: String to add tag to.
+    @param reason: Reason for the error, if needed.
     @return: Formatted string.
     """
-    return f'[color={con.console_message_error}]{msg}[/color]'
+    msg = f'[color={ConsoleMessages.error}]{msg}[/color]'
+    return f"{msg_bold(msg)} - {msg_italics(reason)}" if reason else msg
 
 
-def msg_warn(msg: str) -> str:
+def msg_warn(msg: str, reason: Optional[str] = None) -> str:
     """
     Adds unified warning color tag to Proxyshop console message.
     @param msg: String to add tag to.
+    @param reason: Reason for the warning, if needed.
     @return: Formatted string.
     """
-    return f'[color={con.console_message_warning}]{msg}[/color]'
+    msg = f'[color={ConsoleMessages.warning}]{msg}[/color]'
+    return f"{msg_bold(msg)} - {msg_italics(reason)}" if reason else msg
 
 
 def msg_success(msg: str) -> str:
@@ -68,4 +140,25 @@ def msg_success(msg: str) -> str:
     @param msg: String to add tag to.
     @return: Formatted string.
     """
-    return f'[color={con.console_message_success}]{msg}[/color]'
+    return f'[color={ConsoleMessages.success}]{msg}[/color]'
+
+
+"""
+HEADLESS CONSOLE
+"""
+
+
+class Console:
+    """
+    Replaces the GUI console when running headless.
+    """
+    # TODO: Build out a Headless console class
+
+    @staticmethod
+    def update(msg):
+        print(msg)
+
+    @staticmethod
+    def wait(msg):
+        print(msg)
+        input("Would you like to continue?")
