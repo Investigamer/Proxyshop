df40b8de53da667c19498233c0251ac9d20f0075
MrTeferi <freethoughtleft@gmail.com>
2023-06-25 16:33:31 -0500

    refactor(enums): Add basic lands and transform icons to MTG enums

--- src/enums/mtg.py
+++ src/enums/mtg.py
@@ -13,3 +13,30 @@ class Rarity(StrEnum):
     S = "special"
     B = "bonus"
     T = "timeshifted"
+
+
+class TransformIcons(StrEnum):
+    """Transform icon names."""
+    MOONELDRAZI = "mooneldrazidfc"
+    COMPASSLAND = "compasslanddfc"
+    ORIGINPW = "originpwdfc"
+    CONVERT = "convertdfc"
+    SUNMOON = "sunmoondfc"
+    FAN = "fandfc"
+    MELD = "meld"
+
+
+# Basic land dictionary
+BASIC_LANDS = {
+    "plains": "Basic Land — Plains",
+    "island": "Basic Land — Island",
+    "swamp": "Basic Land — Swamp",
+    "mountain": "Basic Land — Mountain",
+    "forest": "Basic Land — Forest",
+    "wastes": "Basic Land",
+    "snowcoveredplains": "Basic Snow Land — Plains",
+    "snowcoveredisland": "Basic Snow Land — Island",
+    "snowcoveredswamp": "Basic Snow Land — Swamp",
+    "snowcoveredmountain": "Basic Snow Land — Mountain",
+    "snowcoveredforest": "Basic Snow Land — Forest"
+}
