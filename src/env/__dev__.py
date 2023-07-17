c00c0c1af817be5ce5aaebc9578fb9dc9b49bce5
MrTeferi <freethoughtleft@gmail.com>
2023-07-17 17:36:55 -0500

    refactor(env): Remove unused kivy logging ENV, remove KIVY_NO_CONSOLELOG toggle for development mode

--- src/env/__dev__.py
+++ src/env/__dev__.py
@@ -2,11 +2,6 @@
 DEVELOPMENT ENVIRONMENT FLAG
 """
 import sys
-from os import environ
 
 # Disable development flag if building executable release
 development = True if not hasattr(sys, '_MEIPASS') else False
-
-# Disable Kivy output outside development environment
-if not development:
-    environ["KIVY_NO_CONSOLELOG"] = "1"
