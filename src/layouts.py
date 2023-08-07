c4921017648687f20b3e6a5c71f24c9e26df3304
MrTeferi <freethoughtleft@gmail.com>
2023-08-07 05:32:45 -0500

    Update layouts.py

--- src/layouts.py
+++ src/layouts.py
@@ -652,7 +652,7 @@ class PlaneswalkerLayout(NormalLayout):
             index = en_lines[i].find(": ")
             abilities.append({
                 # Activated ability
-                'text': line[index + 2:],
+                'text': line[index + 1:].lstrip(),
                 'icon': en_lines[i][0],
                 'cost': en_lines[i][0:index]
             } if 5 > index > 0 else {
