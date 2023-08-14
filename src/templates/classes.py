b5bb891d5fb380617a2554413550605aa0a60274
MrTeferi <freethoughtleft@gmail.com>
2023-08-14 07:12:44 -0500

    refactor(class-cards): Retool old class card template class into a modifier which passes the class card functionality onto the new vector template

--- src/templates/classes.py
+++ src/templates/classes.py
@@ -21,14 +21,15 @@ import src.format_text as ft
 import src.helpers as psd
 
 
-class ClassTemplate (NormalTemplate):
+class ClassMod (NormalTemplate):
     """
-    * A template for Class cards introduced in Adventures in the Forgotten Realms.
-    * Utilizes some of the same automated positioning techniques as Planeswalker templates.
+    * A template modifier for Class cards introduced in Adventures in the Forgotten Realms.
+    * Utilizes similar automated positioning techniques as Planeswalker templates.
 
     Adds:
         * Level stage groups which contain a cost and level text layer, as well as the divider bar.
         * Level line groups which contain the ability text for each level.
+        * A positioning step to evenly space the abilities and stage dividers.
     """
 
     def __init__(self, layout: ClassLayout, **kwargs):
@@ -37,16 +38,50 @@ class ClassTemplate (NormalTemplate):
         super().__init__(layout, **kwargs)
 
     """
-    LAYERS
+    CLASS METHODS
+    """
+
+    @cached_property
+    def text_layer_methods(self) -> list[Callable]:
+        """Add Class text layers."""
+        funcs = [self.text_layers_classes] if isinstance(self.layout, ClassLayout) else []
+        return [*super().text_layer_methods, *funcs]
+
+    @cached_property
+    def frame_layer_methods(self) -> list[Callable]:
+        """Add Class text layers."""
+        funcs = [self.frame_layers_classes] if isinstance(self.layout, ClassLayout) else []
+        return [*super().frame_layer_methods, *funcs]
+
+    @cached_property
+    def general_methods(self) -> list[Callable]:
+        """Position Class abilities and stage dividers."""
+        funcs = [self.layer_positioning_classes] if isinstance(self.layout, ClassLayout) else []
+        return [*super().general_methods, *funcs]
+
+    """
+    CLASS GROUPS
     """
 
     @cached_property
     def class_group(self) -> LayerSet:
-        return psd.getLayerSet(LAYERS.CLASS, LAYERS.TEXT_AND_ICONS)
+        return psd.getLayerSet(LAYERS.CLASS)
 
     @cached_property
     def stage_group(self) -> LayerSet:
         return psd.getLayerSet(LAYERS.STAGE, self.class_group)
+
+    """
+    CLASS TEXT LAYERS
+    """
+
+    @cached_property
+    def texT_layer_ability(self) -> ArtLayer:
+        return psd.getLayer(LAYERS.TEXT, self.class_group)
+
+    """
+    ABILITY LINES
+    """
 
     @property
     def line_layers(self) -> list[ArtLayer]:
@@ -55,6 +90,10 @@ class ClassTemplate (NormalTemplate):
     @line_layers.setter
     def line_layers(self, value):
         self._line_layers = value
+
+    """
+    STAGE DIVIDER GROUPS
+    """
 
     @property
     def stage_layers(self) -> list[LayerSet]:
@@ -69,53 +108,52 @@ class ClassTemplate (NormalTemplate):
     """
 
     def rules_text_and_pt_layers(self) -> None:
-        self.class_text_layers()
-
-    def post_text_layers(self) -> None:
-        self.class_layer_positioning()
+        """Skip this step for Class cards."""
+        pass
 
     """
-    CLASS CARD METHODS
+    CLASS METHODS
     """
 
-    def class_text_layers(self) -> None:
+    def text_layers_classes(self) -> None:
         """Add and modify text layers relating to Class type cards."""
 
         # Add first static line
-        level_1 = psd.getLayer(LAYERS.TEXT, self.class_group)
-        self.line_layers.append(level_1)
+        self.line_layers.append(self.texT_layer_ability)
         self.text.append(
             text_classes.FormattedTextField(
-                layer=level_1,
+                layer=self.texT_layer_ability,
                 contents=self.layout.class_lines[0]['text']
             )
         )
 
         # Add text fields for each line and class stage
         for i, line in enumerate(self.layout.class_lines[1:]):
-            line_layer = level_1.duplicate()
-            self.active_layer = self.stage_group
-            stage = psd.duplicate_group(f"{self.stage_group.name} {i + 1}")
-            self.line_layers.append(line_layer)
-            self.stage_layers.append(stage)
-            self.text.extend([
-                text_classes.FormattedTextField(
-                    layer=line_layer,
-                    contents=line['text']
-                ),
-                text_classes.FormattedTextField(
-                    layer=psd.getLayer("Cost", stage),
-                    contents=f"{line['cost']}:"
-                ),
-                text_classes.TextField(
-                    layer=psd.getLayer("Level", stage),
-                    contents=f"Level {line['level']}"
-                )
-            ])
-        self.stage_group.visible = False
 
-    def class_layer_positioning(self) -> None:
-        """Positions and sizes class ability layers and divider lines."""
+            # Create a new ability line
+            line_layer = self.texT_layer_ability.duplicate()
+            self.line_layers.append(line_layer)
+
+            # Use existing stage divider or create new one
+            stage = self.stage_group if i == 0 else self.stage_group.duplicate()
+            cost, level = [*stage.artLayers][:2]
+            self.stage_layers.append(stage)
+
+            # Add text layers to be formatted
+            self.text.extend([
+                text_classes.FormattedTextField(layer=line_layer, contents=line['text']),
+                text_classes.FormattedTextField(layer=cost, contents=f"{line['cost']}:"),
+                text_classes.TextField(layer=level, contents=f"Level {line['level']}")
+            ])
+
+    def frame_layers_classes(self) -> None:
+        """Enable frame layers required by Class cards."""
+
+        # None by default
+        pass
+
+    def layer_positioning_classes(self) -> None:
+        """Positions and sizes class ability layers and stage dividers."""
 
         # Core vars
         spacing = self.app.scale_by_dpi(80)
@@ -146,29 +184,8 @@ class ClassTemplate (NormalTemplate):
 """
 
 
-class ClassVectorTemplate (VectorTemplate):
+class ClassVectorTemplate (ClassMod, VectorTemplate):
     """Class template using vector shape layers and automatic pinlines / multicolor generation."""
-
-    def __init__(self, layout: ClassLayout, **kwargs):
-        self._line_layers: list[ArtLayer] = []
-        self._stage_layers: list[LayerSet] = []
-        super().__init__(layout, **kwargs)
-
-    """
-    DETAILS
-    """
-
-    @cached_property
-    def text_layer_methods(self) -> list[Callable]:
-        return [*super().text_layer_methods, self.class_text_layers]
-
-    @cached_property
-    def frame_layer_methods(self) -> list[Callable]:
-        return [*super().frame_layer_methods, self.enable_class_layers]
-
-    @cached_property
-    def general_methods(self) -> list[Callable]:
-        return [*super().general_methods, self.class_layer_positioning]
 
     """
     BOOL
@@ -208,20 +225,21 @@ class ClassVectorTemplate (VectorTemplate):
     """
 
     @cached_property
-    def mode_group(self) -> LayerSet:
-        return psd.getLayerSet(LAYERS.CLASS)
-
-    @cached_property
-    def stage_group(self) -> LayerSet:
-        return psd.getLayerSet(LAYERS.STAGE, self.mode_group)
-
-    @cached_property
     def background_group(self) -> LayerSet:
+        """Support both Nyx and normal Background layers."""
         return psd.getLayerSet(LAYERS.NYX if self.is_nyx else LAYERS.BACKGROUND)
 
     @cached_property
     def crown_group(self) -> LayerSet:
+        """Use inner shape group for Legendary Crown."""
         return psd.getLayerSet(LAYERS.SHAPE, LAYERS.LEGENDARY_CROWN)
+
+    @cached_property
+    def textbox_group(self) -> LayerSet:
+        """Must enable textbox group."""
+        if group := psd.getLayerSet(LAYERS.TEXTBOX):
+            group.visible = True
+            return group
 
     """
     LAYERS
@@ -234,22 +252,6 @@ class ClassVectorTemplate (VectorTemplate):
             f"{self.twins} {LAYERS.BACK}" if self.is_transform and not self.is_front else self.twins,
             self.twins_group)
 
-    @property
-    def line_layers(self) -> list[ArtLayer]:
-        return self._line_layers
-
-    @line_layers.setter
-    def line_layers(self, value):
-        self._line_layers = value
-
-    @property
-    def stage_layers(self) -> list[LayerSet]:
-        return self._stage_layers
-
-    @stage_layers.setter
-    def stage_layers(self, value):
-        self._stage_layers = value
-
     """
     REFERENCES
     """
@@ -261,29 +263,25 @@ class ClassVectorTemplate (VectorTemplate):
     @cached_property
     def textbox_reference(self) -> Optional[ArtLayer]:
         if self.is_front and self.is_flipside_creature:
-            return psd.getLayer(f"{LAYERS.TEXTBOX_REFERENCE} {LAYERS.TRANSFORM_FRONT}", self.mode_group)
-        return psd.getLayer(LAYERS.TEXTBOX_REFERENCE, self.mode_group)
+            return psd.getLayer(f"{LAYERS.TEXTBOX_REFERENCE} {LAYERS.TRANSFORM_FRONT}", self.class_group)
+        return psd.getLayer(LAYERS.TEXTBOX_REFERENCE, self.class_group)
 
     @cached_property
     def textbox_position_reference(self) -> Optional[ArtLayer]:
         return psd.getLayer(LAYERS.ART_FRAME + " Right")
 
     """
-    MASKS
+    BLENDING MASKS
     """
 
     @cached_property
-    def pinlines_mask(self) -> ArtLayer:
-        return psd.getLayer(
-            LAYERS.TRANSFORM_FRONT if self.is_transform and self.is_front else LAYERS.NORMAL,
-            [self.mask_group, LAYERS.PINLINES])
-
-    @cached_property
     def textbox_masks(self) -> list[ArtLayer]:
+        """Blends the textbox colors."""
         return [psd.getLayer(LAYERS.HALF, [self.mask_group, LAYERS.TEXTBOX])]
 
     @cached_property
     def background_masks(self) -> list[ArtLayer]:
+        """Blends the background colors."""
         return [psd.getLayer(LAYERS.HALF, [self.mask_group, LAYERS.BACKGROUND])]
 
     """
@@ -292,7 +290,7 @@ class ClassVectorTemplate (VectorTemplate):
 
     @cached_property
     def border_shape(self) -> Optional[ArtLayer]:
-        # Front face transform border
+        """Support a Normal and Legendary border for front-face Transform."""
         if self.is_transform and self.is_front:
             return psd.getLayer(
                 f"{LAYERS.LEGENDARY if self.is_legendary else LAYERS.NORMAL} {LAYERS.TRANSFORM_FRONT}",
@@ -301,7 +299,7 @@ class ClassVectorTemplate (VectorTemplate):
 
     @cached_property
     def pinlines_shapes(self) -> list[LayerSet]:
-        """Support an additional Legendary pinline shape."""
+        """Support front and back face Transform pinlines, and optional Legendary pinline shape."""
         shapes = [psd.getLayerSet(LAYERS.LEGENDARY, [self.pinlines_group, LAYERS.SHAPE])] if self.is_legendary else []
         return [
             # Normal or Transform pinline
@@ -338,51 +336,34 @@ class ClassVectorTemplate (VectorTemplate):
         ]
 
     """
+    MASKS
+    """
+
+    @cached_property
+    def pinlines_mask(self) -> list[ArtLayer, ArtLayer]:
+        """Mask hiding pinlines effects inside textbox and art frame."""
+        return [
+            psd.getLayer(
+                LAYERS.TRANSFORM_FRONT if self.is_transform and self.is_front else LAYERS.NORMAL,
+                [self.mask_group, LAYERS.PINLINES]),
+            self.pinlines_group
+        ]
+
+    @cached_property
+    def enabled_masks(self) -> list[Union[dict, list, ArtLayer, LayerSet, None]]:
+        """Support a pinlines mask."""
+        return [self.pinlines_mask]
+
+    """
     METHODS
     """
 
     def enable_frame_layers(self) -> None:
+        super().enable_frame_layers()
 
-        # Enable vector shapes
-        self.enable_shape_layers()
-
-        # Color Indicator -> Blend solid colors using layer masks
-        if self.is_type_shifted and self.indicator_group:
-            self.create_blended_solid_color(
-                group=self.indicator_group,
-                colors=self.indicator_colors,
-                masks=self.indicator_masks)
-
-        # Pinlines -> Solid color or gradient layer
-        if self.pinlines_group:
-            self.pinlines_group.visible = True
-            self.pinlines_action(self.pinlines_colors, layer=self.pinlines_group)
-            psd.copy_layer_mask(self.pinlines_mask, self.pinlines_group)
-
-        # Twins -> A static layer
-        if self.twins_layer:
-            self.twins_layer.visible = True
-
-        # Textbox -> Blended texture layers
-        if self.textbox_group:
-            self.textbox_group.visible = True
-            self.create_blended_layer(
-                group=self.textbox_group,
-                colors=self.textbox_colors,
-                masks=self.textbox_masks)
-            psd.merge_group(self.textbox_group)
-            psd.align_horizontal(self.active_layer, self.textbox_position_reference)
-
-        # Background -> Blended texture layers
-        if self.background_group:
-            self.create_blended_layer(
-                group=self.background_group,
-                colors=self.background_colors,
-                masks=self.background_masks)
-
-        # Legendary crown
-        if self.is_legendary:
-            self.enable_crown()
+        # Merge the textbox and shift it to right half
+        psd.merge_group(self.textbox_group)
+        psd.align_horizontal(self.active_layer, self.textbox_position_reference)
 
     def enable_crown(self) -> None:
 
@@ -396,82 +377,16 @@ class ClassVectorTemplate (VectorTemplate):
             vector_masks=[self.pinlines_group]
         )
 
-    def rules_text_and_pt_layers(self) -> None:
-
-        # Skip this step
-        pass
-
     """
     CLASS CARD METHODS
     """
 
-    def enable_class_layers(self):
+    def frame_layers_classes(self):
         """Enable layers relating to Class type cards."""
 
-        # Enable class mode group, disable saga banner
-        self.mode_group.visible = True
+        # Enable class group, disable saga banner
+        self.class_group.visible = True
         psd.getLayerSet("Banner Top").visible = False
-
-    def class_text_layers(self) -> None:
-        """Add and modify text layers relating to Class type cards."""
-
-        # Add first static line
-        level_1 = psd.getLayer(LAYERS.TEXT, self.mode_group)
-        self.line_layers.append(level_1)
-        self.text.append(
-            text_classes.FormattedTextField(
-                layer=level_1,
-                contents=self.layout.class_lines[0]['text']
-            )
-        )
-
-        # Add text fields for each line and class stage
-        for i, line in enumerate(self.layout.class_lines[1:]):
-            line_layer = level_1.duplicate()
-            self.active_layer = self.stage_group
-            stage = psd.duplicate_group(f"{self.stage_group.name} {i + 1}")
-            self.line_layers.append(line_layer)
-            self.stage_layers.append(stage)
-            self.text.extend([
-                text_classes.FormattedTextField(
-                    layer=line_layer,
-                    contents=line['text']
-                ),
-                text_classes.FormattedTextField(
-                    layer=psd.getLayer("Cost", stage),
-                    contents=f"{line['cost']}:"
-                ),
-                text_classes.TextField(
-                    layer=psd.getLayer("Level", stage),
-                    contents=f"Level {line['level']}"
-                )
-            ])
-        self.stage_group.visible = False
-
-    def class_layer_positioning(self) -> None:
-        """Positions and sizes class ability layers and divider lines."""
-
-        # Core vars
-        spacing = self.app.scale_by_dpi(80)
-        spaces = len(self.line_layers) - 1
-        divider_height = psd.get_layer_dimensions(self.stage_layers[0])['height']
-        ref_height = psd.get_layer_dimensions(self.textbox_reference)['height']
-        spacing_total = (spaces * (spacing + divider_height)) + (spacing * 2)
-        total_height = ref_height - spacing_total
-
-        # Resize text items till they fit in the available space
-        ft.scale_text_layers_to_fit(self.line_layers, total_height)
-
-        # Get the exact gap between each layer left over
-        layer_heights = sum([psd.get_text_layer_dimensions(lyr)["height"] for lyr in self.line_layers])
-        gap = (ref_height - layer_heights) * (spacing / spacing_total)
-        inside_gap = (ref_height - layer_heights) * ((spacing + divider_height) / spacing_total)
-
-        # Space Class lines evenly apart
-        psd.spread_layers_over_reference(self.line_layers, self.textbox_reference, gap, inside_gap)
-
-        # Position a class stage between each ability line
-        psd.position_dividers(self.stage_layers, self.line_layers)
 
 
 class UniversesBeyondClassTemplate(ClassVectorTemplate):
@@ -506,14 +421,9 @@ class UniversesBeyondClassTemplate(ClassVectorTemplate):
         # Single color front textures
         return [self.pinlines]
 
-    """
-    LAYERS
-    """
-
     @cached_property
-    def twins_layer(self) -> Optional[ArtLayer]:
-        # Use Back face versions for back side Transform
-        return psd.getLayer(f"{self.twins} Beyond", self.twins_group)
+    def twins_colors(self) -> Optional[str]:
+        return f"{self.twins} Beyond"
 
     """
     GROUPS
@@ -525,4 +435,7 @@ class UniversesBeyondClassTemplate(ClassVectorTemplate):
 
     @cached_property
     def textbox_group(self) -> LayerSet:
-        return psd.getLayerSet(f"{LAYERS.TEXTBOX} Beyond")
+        """Must enable group."""
+        if group := psd.getLayerSet(f"{LAYERS.TEXTBOX} Beyond"):
+            group.visible = True
+            return group
