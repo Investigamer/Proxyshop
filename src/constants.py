84bd20a2782f2026c460c40634e8e2a9e5554391
MrTeferi <freethoughtleft@gmail.com>
2023-11-08 21:30:43 -0600

    refactor(constants): Prefer Path objects over str paths, implement use of data file loaders and dumpers for getting symbol, watermark, and version tracker libraries. Remove relocated data structures, refactor core pathing definitions
    Signed-off-by: MrTeferi <freethoughtleft@gmail.com>

--- src/constants.py
+++ src/constants.py
@@ -4,41 +4,31 @@ GLOBAL CONSTANTS MODULE
 # Standard Library Imports
 import os
 import sys
-import json
-from os import path as osp
 from pathlib import Path
 from threading import Lock
 from typing import Optional
 
-# Third Party Imports
-import yaml
-
-# Establish global root, based on Python or frozen EXE environment
-__PATH_CWD__ = os.getcwd()
-__PATH_ROOT__ = None
-if getattr(sys, 'frozen', False):
-    __PATH_ROOT__ = os.path.dirname(sys.executable)
-elif __file__:
-    __PATH_ROOT__ = os.path.dirname(Path(__file__).parent)
-__PATH_ROOT__ = __PATH_ROOT__ or __PATH_CWD__
+# Establish global root, based on frozen executable or Python
+__PATH_CWD__: Path = Path(os.getcwd())
+__PATH_ROOT__: Optional[Path] = Path(os.path.dirname(sys.executable)) if (
+    getattr(sys, 'frozen', False)
+) else (Path(os.path.dirname(Path(__file__).parent)) if __file__ else __PATH_CWD__)
 
 # Switch to root directory if current directory differs
-if __PATH_CWD__ != __PATH_ROOT__:
+if str(__PATH_CWD__) != str(__PATH_ROOT__):
     os.chdir(__PATH_ROOT__)
 
 # Local Imports
+from src.utils.decorators import suppress_and_return
+from src.utils.files import dump_data_file, load_data_file
 from src.utils.objects import Singleton, PhotoshopHandler
-from src.enums.mtg import mana_color_map
+from src.enums.mtg import mana_color_map, mana_symbol_map, rarity_gradient_map, main_color_map, CardFonts
 from src.enums.layers import LAYERS
 from src.utils.env import ENV
 
 
-# Global app-wide constants class
 class Constants:
-    """
-    Stores global constants that control app behavior.
-    Can be modified within a template class to adjust rendering behavior.
-    """
+    """Stores global constants that control app behavior."""
     __metaclass__ = Singleton
     app = PhotoshopHandler(version=ENV.PS_VERSION)
 
@@ -50,7 +40,6 @@ class Constants:
         "Planeswalker": ["planeswalker"],
         "PW MDFC": ["pw_mdfc_front", "pw_mdfc_back"],
         "PW TF": ["pw_tf_front", "pw_tf_back"],
-        "Basic": ["basic"],
         "Ixalan": ["ixalan"],
         "Mutate": ["mutate"],
         "Prototype": ["prototype"],
@@ -80,30 +69,39 @@ class Constants:
     def load_values(self):
         """Loads default values. Called at launch and between renders to remove any changes made by templates."""
 
-        # Consistent paths used by the app
+        # Main app paths
         self.cwd = __PATH_ROOT__
-        self.path_src = osp.join(self.cwd, 'src')
-        self.path_logs = osp.join(self.cwd, 'logs')
-        self.path_fonts = osp.join(self.cwd, 'fonts')
-        self.path_img = osp.join(self.path_src, 'img')
-        self.path_data = osp.join(self.path_src, 'data')
-        self.path_kv = osp.join(self.path_data, 'kv')
-        self.path_plugins = osp.join(self.cwd, 'plugins')
-        self.path_tests = osp.join(self.path_data, 'tests')
-        self.path_configs = osp.join(self.path_src, 'configs')
-        self.path_templates = osp.join(self.cwd, 'templates')
-        self.path_data_sets = osp.join(self.path_data, 'sets')
-        self.path_config_json_base = osp.join(self.path_data, 'base_settings.json')
-        self.path_config_json_app = osp.join(self.path_data, 'app_settings.json')
-        self.path_config_ini_base = os.path.join(self.path_configs, "base_config.ini")
-        self.path_config_ini_app = os.path.join(self.cwd, "config.ini")
+        self.path_src = Path(self.cwd, 'src')
+        self.path_out = Path(self.cwd, 'out')
+        self.path_logs = Path(self.cwd, 'logs')
+        self.path_fonts = Path(self.cwd, 'fonts')
+        self.path_plugins = Path(self.cwd, 'plugins')
+        self.path_templates = Path(self.cwd, 'templates')
+
+        # Source paths
+        self.path_img = Path(self.path_src, 'img')
+        self.path_data = Path(self.path_src, 'data')
+
+        # Data paths
+        self.path_kv = Path(self.path_data, 'kv')
+        self.path_tests = Path(self.path_data, 'tests')
+        self.path_data_sets = Path(self.path_data, 'mtg/sets')
+        self.path_data_cards = Path(self.path_data, 'mtg/cards')
+
+        # Config paths
+        self.path_config = Path(self.path_data, 'config')
+        self.path_config_ini = Path(self.path_data, 'config_ini')
+        self.path_config_app = Path(self.path_config, 'app.toml')
+        self.path_config_base = Path(self.path_config, 'base.toml')
+        self.path_config_ini_app = Path(self.path_config_ini, "app.ini")
+        self.path_config_ini_base = Path(self.path_config_ini, "base.ini")
 
         # Key files used by the app
-        self.path_scryfall_scan = osp.join(self.path_logs, "card.jpg")
-        self.path_watermarks = osp.join(self.path_data, 'watermarks.json')
-        self.path_version_tracker = osp.join(self.path_data, 'version_tracker.json')
-        self.path_expansion_symbols = osp.join(self.path_data, 'symbols.yaml')
-        self.path_custom_symbols = osp.join(self.path_data, "custom_symbols.yaml")
+        self.path_scryfall_scan = Path(self.path_logs, "card.jpg")
+        self.path_watermarks = Path(self.path_data, 'watermarks.yaml')
+        self.path_version_tracker = Path(self.path_data, 'version_tracker.json')
+        self.path_expansion_symbols = Path(self.path_data, 'symbols.yaml')
+        self.path_custom_symbols = Path(self.path_data, "custom_symbols.yaml")
 
         # Locking handlers for multithreading
         self.lock_file_open = Lock()
@@ -133,166 +131,22 @@ class Constants:
         self.pw_mdfc_front_class = "pw_mdfc_front"
         self.pw_mdfc_back_class = "pw_mdfc_back"
         self.snow_class = "snow"
-        self.basic_class = "basic"
         self.planar_class = "planar"
         self.prototype_class = "prototype"
         self.token_class = "token"
         self.battle_class = "battle"
 
         # Symbol dictionary for NDPMTG font
-        self.symbols = {
-            "{W/P}": "Qp",
-            "{U/P}": "Qp",
-            "{B/P}": "Qp",
-            "{R/P}": "Qp",
-            "{G/P}": "Qp",
-            "{W/U/P}": "Qqyz",
-            "{U/B/P}": "Qqyz",
-            "{B/R/P}": "Qqyz",
-            "{R/G/P}": "Qqyz",
-            "{G/W/P}": "Qqyz",
-            "{W/B/P}": "Qqyz",
-            "{B/G/P}": "Qqyz",
-            "{G/U/P}": "Qqyz",
-            "{U/R/P}": "Qqyz",
-            "{R/W/P}": "Qqyz",
-            "{A}": "oi",
-            "{E}": "e",
-            "{T}": "ot",
-            "{X}": "ox",
-            "{Y}": "oY",
-            "{Z}": "oZ",
-            "{∞}": "o∞",
-            "{0}": "o0",
-            "{1}": "o1",
-            "{2}": "o2",
-            "{3}": "o3",
-            "{4}": "o4",
-            "{5}": "o5",
-            "{6}": "o6",
-            "{7}": "o7",
-            "{8}": "o8",
-            "{9}": "o9",
-            "{10}": "oA",
-            "{11}": "oB",
-            "{12}": "oC",
-            "{13}": "oD",
-            "{14}": "oE",
-            "{15}": "oF",
-            "{16}": "oG",
-            "{17}": "oÅ",
-            "{18}": "oÆ",
-            "{19}": "oÃ",
-            "{20}": "oK",
-            "{W}": "ow",
-            "{U}": "ou",
-            "{B}": "ob",
-            "{R}": "or",
-            "{G}": "og",
-            "{C}": "oc",
-            "{W/U}": "QqLS",
-            "{U/B}": "QqMT",
-            "{B/R}": "QqNU",
-            "{R/G}": "QqOV",
-            "{G/W}": "QqPR",
-            "{W/B}": "QqLT",
-            "{B/G}": "QqNV",
-            "{G/U}": "QqPS",
-            "{U/R}": "QqMU",
-            "{R/W}": "QqOR",
-            "{2/W}": "QqWR",
-            "{2/U}": "QqWS",
-            "{2/B}": "QqWT",
-            "{2/R}": "QqWU",
-            "{2/G}": "QqWV",
-            "{S}": "omn",
-            "{Q}": "ol",
-            "{CHAOS}": "?"
-        }
+        self.symbols = mana_symbol_map.copy()
 
         # Layer names
         self.layers = LAYERS
 
-        # Color reference dictionary
-        self.colors = {
-            'black': [0, 0, 0],
-            'white': [255, 255, 255],
-            'silver': [167, 177, 186],
-            'gold': [166, 135, 75]
-        }
+        # Main color dictionary
+        self.colors = main_color_map.copy()
 
         # Rarity gradient dictionary
-        self.rarity_gradients = {
-            'c': None,
-            'u': [
-                {
-                    "color": [98, 110, 119],
-                    "location": 0,
-                    "midpoint": 50
-                },
-                {
-                    "color": [199, 225, 241],
-                    "location": 2048,
-                    "midpoint": 50
-                },
-                {
-                    "color": [98, 110, 119],
-                    "location": 4096,
-                    "midpoint": 50
-                }
-            ],
-            'r': [
-                {
-                    "color": [146, 116, 67],
-                    "location": 0,
-                    "midpoint": 50
-                },
-                {
-                    "color": [213, 180, 109],
-                    "location": 2048,
-                    "midpoint": 50
-                },
-                {
-                    "color": [146, 116, 67],
-                    "location": 4096,
-                    "midpoint": 50
-                }
-            ],
-            'm': [
-                {
-                    "color": [192, 55, 38],
-                    "location": 0,
-                    "midpoint": 50
-                },
-                {
-                    "color": [245, 149, 29],
-                    "location": 2048,
-                    "midpoint": 50
-                },
-                {
-                    "color": [192, 55, 38],
-                    "location": 4096,
-                    "midpoint": 50
-                }
-            ],
-            't': [
-                {
-                    "color": [98, 45, 118],
-                    "location": 0,
-                    "midpoint": 50
-                },
-                {
-                    "color": [191, 153, 195],
-                    "location": 2048,
-                    "midpoint": 50
-                },
-                {
-                    "color": [98, 45, 118],
-                    "location": 4096,
-                    "midpoint": 50
-                }
-            ]
-        }
+        self.rarity_gradients = rarity_gradient_map.copy()
 
         # Masks
         self.masks = {
@@ -321,12 +175,14 @@ class Constants:
         self.set_symbol_fallback = ""
 
         # Font names
-        self.font_mana = "Proxyglyph"
-        self.font_rules_text = "PlantinMTPro-Regular"
-        self.font_rules_text_bold = "PlantinMTPro-Bold"
-        self.font_rules_text_italic = "PlantinMTPro-Italic"
-        self.font_subtext = "BelerenSmallCaps-Bold"
-        self.font_collector = "Gotham-Medium"
+        self.font_rules_text_italic = CardFonts.RULES_ITALIC
+        self.font_rules_text_bold = CardFonts.RULES_BOLD
+        self.font_rules_text = CardFonts.RULES
+        self.font_collector = CardFonts.COLLECTOR
+        self.font_artist = CardFonts.ARTIST
+        self.font_title = CardFonts.TITLES
+        self.font_pt = CardFonts.TITLES
+        self.font_mana = CardFonts.MANA
 
         # Text Layer formatting
         self.modal_indent = 5.7
@@ -342,84 +198,85 @@ class Constants:
         }
 
     """
-    UTILITY
+    * Utilities
     """
 
     def reload(self):
         """Reloads default attribute values."""
         self.load_values()
 
+    """
+    * Handle: Photoshop
+    """
+
     def refresh_photoshop(self) -> Optional[Exception]:
         """Attempts to refresh the Photoshop object."""
         return self.app.refresh_app()
 
     """
-    VERSION TRACKER
+    * Handle: Version Tracker
     """
 
+    @suppress_and_return({})
     def get_version_tracker(self) -> dict:
         """Get the current version tracker dict."""
-        if not osp.isfile(self.path_version_tracker):
-            # Write a blank version tracker if not found
-            with open(self.path_version_tracker, "w", encoding="utf-8") as f:
-                json.dump({}, f, indent=4)
+        # Write a blank version tracker if not found
+        if not self.path_version_tracker.is_file():
+            dump_data_file({}, self.path_version_tracker)
 
         # Pull the version tracker
-        with open(self.path_version_tracker, "r", encoding="utf-8") as f:
-            try:
-                return json.load(f)
-            except json.decoder.JSONDecodeError:
-                # Data is invalid
-                return {}
+        return load_data_file(self.path_version_tracker)
 
     def update_version_tracker(self):
         """Updates the version tracker json with current dict."""
-        with open(self.path_version_tracker, "w", encoding="utf-8") as vt:
-            json.dump(self.versions, vt, indent=4)
+        dump_data_file(self.versions, self.path_version_tracker)
 
     """
-    EXPANSION SYMBOLS
+    Handle: Expansion Symbols
     """
 
+    @suppress_and_return({})
     def get_expansion_symbols(self) -> dict:
         """
-        Import the expansion symbol library.
+        Import the expansion symbol library (YAML data).
         @return: Dict containing expansion symbol entries.
         """
-        # Import expansion symbol library
-        with open(self.path_expansion_symbols, "r", encoding="utf-8-sig") as f:
-            return yaml.load(f, yaml.Loader)
+        # Check for expansion symbol library
+        if not self.path_expansion_symbols.is_file():
+            dump_data_file({}, self.path_expansion_symbols)
 
+        # Import expansion symbol library
+        return load_data_file(self.path_expansion_symbols)
+
+    @suppress_and_return({})
     def get_custom_symbols(self) -> dict:
         """
-        Import any custom defined expansion symbols.
+        Import any custom defined expansion symbols (YAML data).
         @return: Dict containing custom symbol entries.
         """
         # Check for a custom expansion symbol library
-        if not osp.exists(self.path_custom_symbols):
-            with open(self.path_custom_symbols, "w", encoding="utf-8-sig") as f:
-                json.dump({}, f, indent=4)
+        if not self.path_custom_symbols.is_file():
+            dump_data_file({}, self.path_custom_symbols)
 
         # Pull the custom expansion symbol library
-        with open(self.path_custom_symbols, "r", encoding="utf-8-sig") as f:
-            try:
-                return json.load(f)
-            except json.JSONDecodeError:
-                # Data is invalid
-                return {}
+        return load_data_file(self.path_custom_symbols)
 
     """
-    WATERMARKS
+    Handle: Watermarks
     """
 
+    @suppress_and_return({})
     def get_watermarks(self) -> dict:
         """
-        Import the watermark library.
+        Import the watermark library (YAML data).
         @return: Dict containing watermark entries.
         """
-        # Import expansion symbol library
-        with open(self.path_watermarks, "r", encoding="utf-8") as f:
-            return json.load(f)
+        # Check for a watermark library
+        if not self.path_watermarks.is_file():
+            dump_data_file({}, self.path_watermarks)
+
+        # Import watermark library
+        return load_data_file(self.path_watermarks)
 
 
 # Global instance tracking our constants
