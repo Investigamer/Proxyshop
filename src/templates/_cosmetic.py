ad07261a817634171d275cafb34cb1698c4b64bb
MrTeferi <freethoughtleft@gmail.com>
2023-11-10 16:55:56 -0600

    feat(templates): Implement new CosmeticMod feature allowing for quick cosmetic behavioral addons, e.g. NyxMod, CompanionMod, ExtendedMod, FullartMod, BorderlessMod
    Signed-off-by: MrTeferi <freethoughtleft@gmail.com>

---
+++ src/templates/_cosmetic.py
@@ -0,0 +1,248 @@
+"""
+* Cosmetic Template Class Modifiers
+"""
+# Standard Library Imports
+from functools import cached_property
+from typing import Optional, Callable
+
+# Third Party Imports
+from photoshop.api._artlayer import ArtLayer
+from photoshop.api._layerSet import LayerSet
+
+# Local Imports
+from src.templates._core import BaseTemplate
+from src.templates._vector import VectorTemplate
+from src.enums.layers import LAYERS
+import src.helpers as psd
+
+
+"""
+* Mods that change 'Art Frame' behavior
+"""
+
+
+class ExtendedMod (BaseTemplate):
+    """
+    Modifier for Extended templates.
+
+    Modifies:
+        - 'is_content_aware_enabled': Enabled
+    """
+    frame_suffix = 'Extended'
+
+    """
+    * Bool
+    """
+
+    @property
+    def is_content_aware_enabled(self) -> bool:
+        """Enable content aware fill for Extended Art Frame."""
+        return True
+
+
+class FullartMod (BaseTemplate):
+    """
+    Modifier for Fullart templates.
+
+    Modifies:
+        - 'is_fullart': Enabled
+    """
+    frame_suffix = 'Fullart'
+
+    """
+    * Bool
+    """
+
+    @property
+    def is_fullart(self) -> bool:
+        """Mark this a 'Full Art' card."""
+        return True
+
+
+class BorderlessMod (BaseTemplate):
+    """
+    Modifier for Borderless templates.
+
+    Modifies:
+        - 'is_fullart': Enabled
+        - 'is_content_aware_enabled': Enabled
+        - 'background_layer': None
+    """
+    frame_suffix = 'Borderless'
+
+    """
+    * Bool
+    """
+
+    @property
+    def is_fullart(self) -> bool:
+        """Mark this a 'Full Art' card."""
+        return True
+
+    @property
+    def is_content_aware_enabled(self) -> bool:
+        """Borderless templates should always content aware fill edges."""
+        return True
+
+    """
+    * Layers
+    """
+
+    @property
+    def background_layer(self) -> Optional[ArtLayer]:
+        """Borderless cards have no 'Background' layer."""
+        return
+
+
+class VectorBorderlessMod (BorderlessMod):
+    """
+    Modifier for vectorized Borderless templates.
+
+    Inherits:
+        - 'BorderlessMod'
+    Modifies:
+        - 'background_group': None
+    """
+
+    """
+    * Groups
+    """
+
+    @property
+    def background_group(self) -> Optional[LayerSet]:
+        """Borderless cards have no 'Background' group."""
+        return
+
+
+"""
+* Mods that add additional frame elements
+"""
+
+
+class NyxMod (BaseTemplate):
+    """
+    Modifier for 'Nyxtouched' supported templates.
+
+    Modifies:
+        - 'is_hollow_crown': Enabled for Nyxtouched cards
+        - 'background_layer': Use 'Nyx' layer for Nyxtouched cards
+    """
+
+    """
+    * Bool
+    """
+
+    @cached_property
+    def is_hollow_crown(self) -> bool:
+        """Enable hollow crown for Nyx cards."""
+        if self.is_nyx:
+            return True
+        return super().is_hollow_crown
+
+    """
+    * Layers
+    """
+
+    @cached_property
+    def background_layer(self) -> Optional[ArtLayer]:
+        """Try finding a Nyx background layer if the card is a 'Nyxtouched' frame."""
+        if self.is_nyx:
+            if layer := psd.getLayer(self.background, LAYERS.NYX):
+                return layer
+        return super().background_layer
+
+
+class VectorNyxMod (NyxMod, VectorTemplate):
+    """
+    Modifier for vectorized 'Nyxtouched' supported templates.
+
+    Inherits:
+        - 'NyxMod'
+    Adds:
+        - 'background_group': Use 'Nyx' group if card is Nyxtouched.
+    """
+
+    @cached_property
+    def background_group(self) -> Optional[LayerSet]:
+        """Try finding a Nyx background group if the card is a 'Nyxtouched' frame."""
+        if self.is_nyx:
+            if layer := psd.getLayerSet(LAYERS.NYX):
+                return layer
+        return super().background_group
+
+
+class CompanionMod (BaseTemplate):
+    """
+    Modifier for 'Companion' supported templates.
+
+    Modifies:
+        - 'is_hollow_crown': Enabled for Companion cards
+    Adds:
+        - 'companion_layer': Defines the Companion texture layer if card is Companion
+        - 'enable_companion_layers': Called when card is a Companion
+    """
+
+    @cached_property
+    def frame_layer_methods(self) -> list[Callable]:
+        """Add companion layers step."""
+        funcs = [self.enable_companion_layers] if self.is_companion else []
+        return [*super().frame_layer_methods, *funcs]
+
+    """
+    * Bool
+    """
+
+    @cached_property
+    def is_hollow_crown(self) -> bool:
+        """Enable hollow crown for Companion cards."""
+        if self.is_companion:
+            return True
+        return super().is_hollow_crown
+
+    """
+    * Layers
+    """
+
+    @cached_property
+    def companion_layer(self) -> Optional[ArtLayer]:
+        """Companion inner crown layer."""
+        return psd.getLayer(self.pinlines, LAYERS.COMPANION)
+
+    """
+    * Companion Methods
+    """
+
+    def enable_companion_layers(self) -> None:
+        """Enable Hollow Crown companion texture."""
+        if self.is_legendary and self.companion_layer:
+            self.companion_layer.visible = True
+
+
+class VectorCompanionMod (CompanionMod, VectorTemplate):
+    """
+    Modifier for vectorized 'Companion' supported templates.
+
+    Inherits:
+        - 'CompanionMod'
+    Adds:
+        - 'companion_group': Defines the group containing Companion textures if card is Companion
+    Modifies:
+        - 'enable_companion_layers': Uses 'create_blended_layer' to blend companion textures.
+    """
+
+    @cached_property
+    def companion_group(self) -> Optional[LayerSet]:
+        """Group containing Companion inner crown textures."""
+        return psd.getLayerSet(LAYERS.COMPANION)
+
+    """
+    * Companion Methods
+    """
+
+    def enable_companion_layers(self) -> None:
+        """Generate blended hollow crown Companion texture."""
+        if self.is_legendary and self.companion_group:
+            self.create_blended_layer(
+                group=self.companion_group,
+                colors=self.crown_colors,
+                masks=self.crown_masks)
