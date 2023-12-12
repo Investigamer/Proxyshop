9cae2587f6bb5187a0acc0bb51889864cd15eea2
Investigamer <freethoughtleft@gmail.com>
2023-12-12 17:32:59 -0600

    refactor(utils/image): Convert image to RGB to prevent potential errors
    Signed-off-by: Investigamer <freethoughtleft@gmail.com>

--- src/utils/image.py
+++ src/utils/image.py
@@ -9,18 +9,24 @@ from PIL import Image
 from PIL.Image import Resampling
 
 
-def downscale_image(path: Path, **kwargs) -> None:
-    """
-    Downscale an image to max width of MAX_WIDTH.
-    @param path: Path to the image.
-    @keyword width (int): Maximum width, default: 3264
-    @keyword optimize (bool): Whether to use Pillow optimize, default: True
-    @keyword quality (int): JPEG quality, 1-100, default: 3264
-    @keyword resample (Resampling): Resampling algorithm, default: LANCZOS
+def downscale_image(path: Path, **kwargs) -> bool:
+    """Downscale an image to max width of MAX_WIDTH.
+
+    Args:
+        path: Path to the image.
+
+    Keyword Args:
+        width (int): Maximum width, default: 3264
+        optimize (bool): Whether to use Pillow optimize, default: True
+        quality (int): JPEG quality, 1-100, default: 3264
+        resample (Resampling): Resampling algorithm, default: LANCZOS
+
+    Returns:
+        True if successful, otherwise False.
     """
     # Establish our source and destination directories
     path_out = Path(path.parent, 'compressed')
-    path_out.mkdir(mode=711, parents=True, exist_ok=True)
+    path_out.mkdir(mode=777, parents=True, exist_ok=True)
     save_path = Path(
         path_out, kwargs.get('name', path.name)
     ).with_suffix('.jpg')
@@ -33,6 +39,9 @@ def downscale_image(path: Path, **kwargs) -> None:
     # Open the image, get dimensions
     with Image.open(path) as image:
 
+        # Convert to RGB
+        image.convert('RGB')
+
         # Calculate dimensions
         width, height = image.size
         if width > max_width:
@@ -41,8 +50,12 @@ def downscale_image(path: Path, **kwargs) -> None:
                 resample=kwargs.get('resample', Resampling.LANCZOS))
 
         # Save the new image
-        image.save(
-            fp=save_path,
-            format='JPEG',
-            quality=quality,
-            optimize=optimize)
+        try:
+            image.save(
+                fp=save_path,
+                format='JPEG',
+                quality=quality,
+                optimize=optimize)
+        except OSError:
+            return False
+        return True
