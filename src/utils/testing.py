2472ed04c53add736737d7e27818c0e86d5a3c61
Investigamer <freethoughtleft@gmail.com>
2023-12-29 18:11:31 -0600

    refactor(test_execution_time): Refactor execution time testing utility
    Signed-off-by: Investigamer <freethoughtleft@gmail.com>

--- src/utils/testing.py
+++ src/utils/testing.py
@@ -4,7 +4,7 @@
 # Standard Library Imports
 from time import perf_counter
 from operator import itemgetter
-from typing import Optional, Union, Callable
+from typing import Optional, Union, Callable, Any
 
 # Third Party
 import colorama
@@ -38,78 +38,55 @@ def time_function(func: Callable) -> Callable:
 
 
 def test_execution_time(
-    new_func: Callable,
-    old_func: Callable,
+    funcs: list[tuple[Callable, list[Any]]],
     iterations=1000,
-    args=None,
-    args_old=None,
-    check_result=True,
     reset_func: Optional[Callable] = None
 ) -> None:
     """Test the execution time of a new function against an older function.
 
-    Todo:
-        Needs a complete rewrite.
-
     Args:
-        new_func: New callable function to test.
-        old_func: Older callable function to compare against.
-        iterations: How many times to run these functions, higher means better sample size.
-        args: Args to pass to the newer function, and older function unless specified in args_old.
-        args_old: Args to pass to the older function.
-        check_result: Whether to check if results match.
+        funcs: List of funcs to test execution time for, along with their args.
         reset_func: Optional function to call to reset app state between actions.
     """
+    # Track results
+    res: dict[str, Union[None, int, float, str, list]] = {
+        'value': None, 'average': 0, 'times': [], 'name': ''}
+
     # Test configuration
-    args = args or []
-    args_old = args_old or args
     colorama.init(autoreset=True)
-    results: list[dict[str, Union[None, int, float, str, list]]] = [
-        {
-            'value': None,
-            'average': 0,
-            'times': [],
-            'type': 'Newer'
-        },
-        {
-            'value': None,
-            'average': 0,
-            'times': [],
-            'type': 'Older'
-        }
-    ]
+    results: list[dict[str, Union[None, int, float, str, list]]] = []
 
-    # Test new functionality
-    for i in range(iterations):
-        s = perf_counter()
-        results[0]['value'] = new_func(*args)
-        results[0]['times'].append(perf_counter()-s)
-        if reset_func:
-            reset_func()
-    results[0]['average'] = sum(results[0]['times'])/len(results[0]['times'])
-
-    # Test old functionality
-    for i in range(iterations):
-        s = perf_counter()
-        results[1]['value'] = old_func(*args_old)
-        results[1]['times'].append(perf_counter()-s)
-        if reset_func:
-            reset_func()
-    results[1]['average'] = sum(results[1]['times'])/len(results[1]['times'])
+    # Test each function
+    for func, args in funcs:
+        r = res.copy()
+        for i in range(iterations):
+            s = perf_counter()
+            r['value'] = func(*args)
+            r['times'].append(perf_counter()-s)
+            if reset_func:
+                reset_func()
+        r['average'] = sum(r['times'])/len(r['times'])
+        r['name'] = func.__name__
+        results.append(r.copy())
 
     # Report results
-    for i, res in enumerate(results):
-        print(f"{res['type']} method: {res['average']}")
+    results, compare = sorted(results, key=itemgetter('average')), 0
+    for i, r in enumerate(results):
+        if i == 0:
+            compare = r['average']
+            print(f"{Fore.GREEN}{r['name']}: {r['average']}")
+            continue
+        delta = r['average'] - compare
+        percent = round(delta / ((r['average'] + compare) / 2) * 100, 2)
+        print(f"{r['name']}: {r['average']} ({percent}% Slower)")
 
-    # Compare results
-    final = sorted(results, key=itemgetter('average'))
-    slower = final[1]['average']
-    faster = final[0]['average']
-    delta = slower - faster
-    percent = round(delta/((slower + faster)/2) * 100, 2)
-    print(f"{Fore.GREEN}The {final[0]['type']} method is {percent}% faster!")
-    if check_result:
-        print(f"Results check: {Fore.GREEN+'SUCCESS' if final[0]['value'] == final[1]['value'] else Fore.RED+'FAILED'}")
-        if final[0]['value']:
-            print(final[0]['value'])
-            print(final[1]['value'])
+    # Check if all values were the same
+    check, values = f'{Fore.GREEN}SUCCESS', []
+    for i, r in enumerate(results):
+        if i == 0:
+            values.append(r['value'])
+            continue
+        if r['value'] not in values:
+            check = f'{Fore.RED}FAILED'
+            break
+    print(f"Values check: {check}")
