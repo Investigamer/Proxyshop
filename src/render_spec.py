bb32675a7a058408babd6508c02ba6c8ed857d2c
Mal <vingine.dev@gmail.com>
2024-01-06 13:40:03 +0100

    Add groups in a render-spec

--- src/render_spec.py
+++ src/render_spec.py
@@ -64,24 +64,57 @@ def parse_render_spec(file_path: Path, logger: Optional[Any] = None) -> RenderSp
 
     # Now find all the cards and parse them by using the configs
     cards = []
+    groups = []
     for l in card_lines:
+        # Entering a group
+        if l.startswith('{'):
+            groups.append([])
+            l = l[1:].strip()
+            if not l:
+                continue
+
         parts = list(map(str.strip, l.split('|')))
-        full_card_spec = parts[0]
+        full_line_spec = parts[0]
         used_configs = parts[1:]
         invalid_configs = [c for c in used_configs if c not in configs]
         if invalid_configs:
             if logger:
-                logger.update(f'Card ignored due to unknown render spec configurations: {full_card_spec.split()[0]}')
+                logger.update(f'Card ignored due to unknown render spec configurations: {full_line_spec.split()[0]}')
                 for c in invalid_configs:
                     logger.update(f'\nUnknown render spec configuration: {c}')
             continue
         for c in used_configs:
             spec_info = configs[c]['info']
-            full_card_spec += f' {spec_info}'
+            full_line_spec += f' {spec_info}'
 
-        # Pretend this is a file and parse that
-        full_card_path = file_path.with_stem(full_card_spec)
-        cards.append(parse_card_info(full_card_path))
+        def append_card(card_spec):
+            # Pretend this is a file and parse that
+            full_card_path = file_path.with_stem(card_spec)
+            cards.append(parse_card_info(full_card_path))
+        
+        # If part of a group we need to just accumulate
+        if groups:
+            if l.startswith('}'):
+                # The group ended, so we assign this configuration to all the cards in it
+                full_line_spec = full_line_spec[1:].strip()
+                ended_group = groups.pop()
+                ended_group = [c + f' {full_line_spec}' for c in ended_group]
+
+                # Replace special variables
+                ended_group = [c.replace('${GROUP_INDEX}', str(i)) for (i, c) in enumerate(ended_group)]
+                
+                if groups:
+                    # If this was a nested group we just put these into the outer group
+                    groups[-1].extend(ended_group)
+                else:
+                    # Otherwise append to the render spec
+                    for card in ended_group:
+                        append_card(card)
+            else:
+                # Append the card to the group
+                groups[-1].append(full_line_spec)
+        else:
+            append_card(full_line_spec)
 
     # Return dictionary
     return {
