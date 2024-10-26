714ae913037dadd53a0840471ab75beae36614af
Chris <hexproofsystems@gmail.com>
2024-10-26 18:37:32 -0500

    refactor(env): Rework AppEnvironment to use DynaConf with yml instead of .env, add more PATH fields, implement use of Hexproof's Meta schema
    Signed-off-by: Chris <hexproofsystems@gmail.com>

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,13 @@
 """
 * Load Global Application State
 """
+# Standard Library Imports
+import sys
+
+# Third Party Imports
+from dynaconf import Validator
+from omnitils.files import get_project_version
+
 # Local Imports
 from .console import TerminalConsole
 from ._config import AppConfig
@@ -12,8 +19,23 @@ from src.utils.adobe import PhotoshopHandler
 * Globally Loaded Objects
 """
 
-# Global environment object
-ENV = AppEnvironment()
+# Global environment object (dynaconf)
+ENV = AppEnvironment(
+    envvar_prefix='PROXYSHOP',
+    settings_files=[PATH.SRC_DATA_ENV, PATH.SRC_DATA_ENV_DEFAULT],
+    validators=[
+        Validator('API_GOOGLE', cast=str, default=''),
+        Validator('API_AMAZON', cast=str, default=''),
+        Validator('PS_ERROR_DIALOG', cast=bool, default=False),
+        Validator('PS_VERSION', cast=AppEnvironment.string_or_none, default=None),
+        Validator('HEADLESS', cast=bool, default=False),
+        Validator('DEV_MODE', cast=bool, default=bool(not hasattr(sys, '_MEIPASS'))),
+        Validator('TEST_MODE', cast=bool, default=False),
+        Validator('VERSION', cast=str, default=get_project_version(PATH.PROJECT_FILE)),
+        Validator('FORCE_RELOAD', cast=bool, default=False)
+    ],
+    apply_default_on_none=True
+)
 
 # Global constants object
 CON = AppConstants()
